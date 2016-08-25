f1fc0f3ef7deb7653182e2b98f8b94ef77be9885
charlieperson <charlie.person@hyperisland.se>
2016-08-25 12:21:39 -0500

    Completes first working class challenge

---
+++ chapterTwo/firstClass/DogTestDrive.java
@@ -0,0 +1,9 @@
+class DogTestDrive {
+  public static void main(String[] args) {
+    Dog myDog = new Dog();
+    myDog.size = 50;
+    myDog.breed = "chocoLab";
+    myDog.name = "Woofy";
+    myDog.bark();
+  }
+}
